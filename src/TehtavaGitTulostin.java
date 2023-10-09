52d58f7f9bfb3c9a77594d916060cd00d69de382
hanskihamk03 <hanna.k.honkonen@gmail.com>
2023-10-09 09:46:50 +0300

    1. commit

--- src/TehtavaGitTulostin.java
+++ src/TehtavaGitTulostin.java
@@ -2,7 +2,27 @@
 public class TehtavaGitTulostin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hei olen Tulostin-ohjelma"); 
+		System.out.print("Ohjelman tekijä: "); 
+		String tekija = "Hanna"; 
+		System.out.println(tekija); 
+
+		//
+		double luku1 =6.0; 
+		double luku2 =7.0;
+		double tulo1 =luku1 * luku2;
+		double summa = luku1 + luku2;
+		double erotus = luku1 - luku2;
+		double jako = luku1 / luku2;
+		
+		// homma toimi :) luku1 eli 6 tulostui omana muuttujan arvona 
+
+		System.out.println("Luku1-muuttujan arvo on " + luku1); 
+
+		System.out.println("Luku2-muuttujan arvo on " + luku2);  
+ 
+		System.out.print(luku1 + " * " + luku2 + " = " + tulo1);
 
 	}
 
